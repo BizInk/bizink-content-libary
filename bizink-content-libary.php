1addc41ba1ae62583fdb95285882788e8643100a
Jayden Major <jaydenmajor@gmail.com>
2026-07-27 21:44:36 +1000

    Version

--- bizink-content-libary.php
+++ bizink-content-libary.php
@@ -4,7 +4,7 @@
  Plugin URI:  https://portal.bizinkonline.com/
  Description: 
  Author:      Jayden Major
- Version:     0.1.1
+ Version:     0.1.2
  Author URI:  https://portal.bizinkonline.com/
  Text Domain: bizink-content
  License:     GPLv2 or later
